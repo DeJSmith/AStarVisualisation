2320bb2c4373bb4f3b1e3ea92b230053373f2d20
dan <djssmith@hotmail.co.nz>
2019-09-26 14:06:36 +1200

    initial upload

---
+++ AStarViz/Maps/MapFactory.js
@@ -0,0 +1,33 @@
+/*
+
+    The map factory produces a map using one of 3 map generations methods at random
+
+*/
+
+function MapFactory()
+{
+    this.maps = [];
+    this.maps.push(SearchMap); // Simple random map
+    this.maps.push(BspMap); // BSP rogue-like map
+    this.maps.push(MazeMap); //Maze like map
+    this.maps.push(SearchMap); 
+    this.maps.push(BspMap); 
+    this.maps.push(MazeMap);
+    
+    
+    this.shuffleMaps = function(){
+        this.maps.sort(() => Math.random() - 0.5);
+    }
+
+    this.getMap = function(cols, rows, x, y, w, h, allowDiagonals, percentObstacles)
+    {
+        this.shuffleMaps();
+        
+        if(this.maps.length == 0) return undefined;
+
+        var percentObstacles = 0.5;
+
+        return new this.maps[0](cols, rows, x, y, w, h, allowDiagonals, percentObstacles);
+    }
+
+}
